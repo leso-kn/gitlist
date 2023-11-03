85f687358ff6ec240281410ffc7fc52ab8f96aa3
leso-kn <info@lesosoftware.com>
2023-11-03 14:09:03 +0100

    Update bootstrap to v5.3, Add dark theme

--- assets/viewers/ace/main.js
+++ assets/viewers/ace/main.js
@@ -1,4 +1,8 @@
+const darkModePreference = window.matchMedia("(prefers-color-scheme: dark)");
+
 var ace = require('ace-builds/src-min-noconflict/ace');
+require('ace-builds/src-min-noconflict/theme-' + editor_theme);
+require('ace-builds/src-min-noconflict/theme-' + editor_theme_dark);
 require('ace-builds/webpack-resolver');
 
 window.addEventListener('load', function () {
@@ -8,10 +12,16 @@ window.addEventListener('load', function () {
     return;
   }
 
-  ace.edit(editor, {
+  editor = ace.edit(editor, {
     mode: 'ace/mode/' + editor.dataset.mode,
     maxLines: 50,
     minLines: 10,
     fontSize: 16,
+    showPrintMargin: false,
+    theme: 'ace/theme/' + (darkModePreference.matches ? editor_theme_dark : editor_theme),
+  });
+
+  darkModePreference.addEventListener("change", e => {
+    editor.setTheme('ace/theme/' + (e.matches ? editor_theme_dark : editor_theme))
   });
 });
